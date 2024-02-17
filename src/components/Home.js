7e22ffc91deeff1e4e5e8ba2ba3b8fe9ca29eacd
Puru-Rana <pururana24@gmail.com>
2024-02-16 23:32:38 -0500

    2: Fix

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,7 +14,7 @@ const Home = () => {
               I'm a Full Stack Developer
             </h2>
             <p className="text-gray-500 py-4 max-w-md">
-              I have 3 months of experience building and desgining software.
+              I have 6 months of experience building and desgining software.
               Currently, I love to work on web application using technologies
               like React, Tailwind, and Next JS .
             </p>
